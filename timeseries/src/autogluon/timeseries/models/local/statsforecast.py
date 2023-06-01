801f6900907acb38b2a33195b96d1a981bd3bb9b
Oleksandr Shchur <oleks.shchur@gmail.com>
2023-06-01 08:46:32 +0200

    Revert "[timeseries] Implement prediction caching and refactor prediction logic in AbstractTimeSeriesTrainer (#3237)"
    This reverts commit ac9c02630255bcc13e20676e05b21d0b8070ddc9.

--- timeseries/src/autogluon/timeseries/models/local/statsforecast.py
+++ timeseries/src/autogluon/timeseries/models/local/statsforecast.py
@@ -244,8 +244,6 @@ class ThetaModel(AbstractStatsForecastModel):
         When set to -1, all CPU cores are used.
     """
 
-    max_ts_length = 3000
-
     allowed_local_model_args = [
         "decomposition_type",
         "seasonal_period",
